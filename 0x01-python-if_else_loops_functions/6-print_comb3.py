add247c410258a476696de6806d1f8e538e13ab4
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-08-10 00:34:20 +0200

    All possible different combinations of two digits

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-for i in range(0, 9):
+for i in range(0, 10):
     for j in range(i, 10):
+        if i == j:
+            continue
         print("{}{}".format(i, j), end=", ")
 print("89")
