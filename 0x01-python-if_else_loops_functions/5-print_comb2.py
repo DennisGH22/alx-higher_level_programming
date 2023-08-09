38914cf94ac84fe8f1ca109e1f0db8fd546162c1
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-08-09 22:50:14 +0200

    00...99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 100):
-    if len(str(i)) < 2:
-        i = '0' + str(i)
     if i == 99:
-        print("{}".format(i))
+        print("99")
         continue
-    print("{}".format(i), end=", ")
+    print("{:02d}".format(i), end=", ")
