5d736dd05ea011e55d743ed1b7db0403e908094f
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-08-09 17:06:13 +0200

    To bytecode

--- 0x00-python-hello_world/102-magic_calculation.py
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
+	"""Perform a magic calculation.
+
+	Args:
+		a (int): The base value.
+		b (int): The exponent value.
+
+	Returns:
+		int: The result of the magic calculation.
+	"""
 	return (98 + a ** b)
