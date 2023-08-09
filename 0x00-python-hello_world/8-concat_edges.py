ec45421af13f7a8dd5530dd8a57948f8ae1a124c
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-08-09 13:12:47 +0200

    Create a new sentence

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str1 = str[39:-62]
+str2 = str[107:-17]
+str = str1 + str2 + str[:6]
+print(str)
