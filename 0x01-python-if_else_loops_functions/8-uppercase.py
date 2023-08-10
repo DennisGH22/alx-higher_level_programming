b056e9a64ac930395effbcef5d88973fa46019e0
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-08-10 02:01:58 +0200

    To uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
+    uppercase_str = ""
     for i in str:
         if ord('a') <= ord(i) <= ord('z'):
             i = chr(ord(i) - 32)
-            print("{}".format(i), end="")
-        else:
-            print("{}".format(i), end="")
+        uppercase_str += "{}".format(i)
+    print(uppercase_str)
