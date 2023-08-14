0ab969abce76e9e2fa10bef1818e6bfc6a836720
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-08-14 23:45:04 +0200

    Print a matrix of integers

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    print('\n'.join([' '.join(['{:2}'.format(j) for j in i]) for i in matrix]))
+    for i in matrix:
+        print(' '.join(map(str, i)))
