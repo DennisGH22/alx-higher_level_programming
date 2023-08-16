7d2a3d3f9fe545804b27ef7dc080266d86ce1f26
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-08-16 21:09:26 +0200

    Initial commit

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-from add_0 import add
-
-if __name__ == "__main__":
-    print("{} = {}".format("1 + 2", add(1, 2)))
+if __name__ == "__main__": 
+    from add_0 import add
+    print(f"1 + 2 = {add(1, 2)}")
