d36003eca417b821a5bd3fe411a110a5842135c1
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-08-16 22:06:28 +0200

    Number of and the list of its arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+
+    elements_len = len(argv) - 1
+    argument_str = "argument" if elements_len == 1 else "arguments"
+    argument_end = "." if elements_len == 0 else ":"
+
+    print("{} {}{}".format(elements_len, argument_str, argument_end))
+
+    for i, v in enumerate(argv[1:], start=1):
+        print(f"{i}: {v}")
