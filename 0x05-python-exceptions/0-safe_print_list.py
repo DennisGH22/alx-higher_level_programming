b768c623e6a7c0c1a7c190f07f9f5d53d76a1a27
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-08-28 18:57:49 +0200

    Initial commit

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
         count = 0
         for element in my_list:
             if count < x:
-                print(element, end='')
+                print("{}".format(element), end="")
                 count += 1
             else:
                 break
