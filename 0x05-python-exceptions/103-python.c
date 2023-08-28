435d2a08acd026e27fe87344bf49234fcd6fc7af
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-08-28 20:50:26 +0200

    Print basic info about Python lists

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -1,108 +1,105 @@
 #include <Python.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-void print_python_bytes(PyObject *obj);
-void print_python_float(PyObject *obj);
-void print_python_list(PyObject *obj);
+/* Function prototypes */
+void print_python_bytes(PyObject *p);
+void print_python_float(PyObject *p);
+void print_python_list(PyObject *p);
 
+/* Set stdout to unbuffered mode */
 void set_unbuffered_stdout(void)
 {
 	setbuf(stdout, NULL);
 }
 
+/* Print error message for invalid object type */
 void print_error_message(const char *type)
 {
 	fprintf(stderr, "[ERROR] Invalid %s Object\n", type);
 }
 
-void print_bytes_info(PyBytesObject *bytes)
+/* Print information about Python bytes object */
+void print_python_bytes(PyObject *p)
 {
-	size_t bytes_to_print = PyBytes_Size(bytes) >= 10 ? 10 : PyBytes_Size(bytes);
+	size_t size, i;
+	const char *str;
+	PyBytesObject *bytes;
 
-	printf("  size: %ld\n  trying string: %s\n", PyBytes_Size(bytes), bytes->ob_sval);
+	set_unbuffered_stdout();
+	printf("[.] bytes object info\n");
+
+	if (!PyBytes_Check(p))
+	{
+		print_error_message("Bytes");
+		return;
+	}
+
+	bytes = (PyBytesObject *)p;
+	size = PyBytes_Size(p);
+
+	printf("  size: %ld\n  trying string: %s\n", size, bytes->ob_sval);
+
+	size_t bytes_to_print = (size >= 10) ? 10 : size;
 	printf("  first %zd bytes: ", bytes_to_print);
 
-	for (size_t i = 0; i < bytes_to_print; i++)
-	{
+	for (i = 0; i < bytes_to_print - 1; i++)
 		printf("%02hhx ", bytes->ob_sval[i]);
-	}
-	printf("\n");
+	printf("%02hhx\n", bytes->ob_sval[i]);
 }
 
-void print_float_info(PyFloatObject *flt)
+/* Print information about Python float object */
+void print_python_float(PyObject *p)
 {
-	char *str = PyOS_double_to_string(flt->ob_fval, 'r', 0, Py_DTSF_ADD_DOT_0, NULL);
+	double value;
+	char *str;
+
+	set_unbuffered_stdout();
+	printf("[.] float object info\n");
+
+	if (!PyFloat_Check(p))
+	{
+		print_error_message("Float");
+		return;
+	}
+
+	value = ((PyFloatObject *)(p))->ob_fval;
+	str = PyOS_double_to_string(value, 'r', 0, Py_DTSF_ADD_DOT_0, NULL);
 	printf("  value: %s\n", str);
 	PyMem_Free(str);
 }
 
-void print_list_info(PyListObject *list)
+/* Print information about Python list object */
+void print_python_list(PyObject *p)
 {
-	size_t size = PyList_GET_SIZE(list);
-	size_t allocated = list->allocated;
+	size_t size, i;
+	const char *type_name;
+	PyListObject *list;
 
+	set_unbuffered_stdout();
 	printf("[*] Python list info\n");
-	printf("[*] Size of the Python List = %ld\n[*] Allocated = %li\n", size, allocated);
 
-	for (size_t i = 0; i < size; i++)
+	if (!PyList_Check(p))
+	{
+		print_error_message("List");
+		return;
+	}
+
+	list = (PyListObject *)p;
+	size = PyList_GET_SIZE(p);
+
+	printf("[*] Size of the Python List = %ld\n", size);
+
+	for (i = 0; i < size; i++)
 	{
 		PyObject *item = list->ob_item[i];
-		const char *type_name = item->ob_type->tp_name;
-
+		type_name = item->ob_type->tp_name;
 		printf("Element %ld: %s\n", i, type_name);
 
 		if (strcmp(type_name, "bytes") == 0)
-		{
 			print_python_bytes(item);
-		}
 		else if (strcmp(type_name, "float") == 0)
-		{
 			print_python_float(item);
-		}
-	}
-}
-
-void print_python_bytes(PyObject *obj)
-{
-	set_unbuffered_stdout();
-	printf("[.] bytes object info\n");
-
-	if (PyBytes_Check(obj))
-	{
-		print_bytes_info((PyBytesObject *)obj);
-	}
-	else
-	{
-		print_error_message("Bytes");
-	}
-}
-
-void print_python_float(PyObject *obj)
-{
-	set_unbuffered_stdout();
-	printf("[.] float object info\n");
-
-	if (PyFloat_Check(obj))
-	{
-		print_float_info((PyFloatObject *)obj);
-	}
-	else
-	{
-		print_error_message("Float");
-	}
-}
-
-void print_python_list(PyObject *obj)
-{
-	set_unbuffered_stdout();
-	printf("[*] Python list info\n");
-
-	if (PyList_Check(obj))
-	{
-		print_list_info((PyListObject *)obj);
-	}
-	else
-	{
-		print_error_message("List");
 	}
 }
