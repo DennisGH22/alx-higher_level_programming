6af08ae4044b44875b413ced4d58a52572e18fea
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-08-28 20:17:00 +0200

    Safe function

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+""" safe_function: Executes a function safely. """
+
 def safe_function(fct, *args):
     try:
         return fct(*args)
-    except Exception as e: 
+    except Exception as e:
         import sys
 
         print("Exception: {}".format(e), file=sys.stderr)
