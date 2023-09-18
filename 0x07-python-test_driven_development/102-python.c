080c8c3abbae1874e0ff265469a0ca8d83348370
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-09-18 22:16:50 +0200

    Python Strings

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -22,9 +22,9 @@ void print_python_string(PyObject *p)
     printf("[.] string object info\n");
 
 	if (PyUnicode_IS_COMPACT_ASCII(p))
-		printf("type: compact ascii\n");
+		printf(" type: compact ascii\n");
 	else
-		printf("type: compact unicode object\n");
+		printf(" type: compact unicode object\n");
 
     printf("  length: %zd\n", length);
     printf("  value: %ls\n", PyUnicode_AsWideCharString(p, &length));
