111ab347e06f40f045a38b0b3a7dba77d05bbc81
Tebogo Ndaba <dennistebogo@gmail.com>
2023-09-18 11:26:49 +0200

    Initial commit

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" Add two integers. """
+
+
+def add_integer(a, b=98):
+    """
+    Add two integers.
+
+    :param a: An integer or a float to be added.
+    :param b: An integer or a float to be added. Defaults to 98.
+    :return: The addition of a and b as an integer.
+    :raises TypeError: If a or b is not an integer or a float.
+    """
+    if not (isinstance(a, (int, float))):
+        raise TypeError("a must be an integer")
+    
+    if not (isinstance(b, (int, float))):
+        raise TypeError("b must be an integer")
+
+    if isinstance(a, float):
+        a = int(a)
+
+    if isinstance(b, float):
+        b = int(b)
+
+    result = a + b
+
+    return result
