a77175f5df584a99efc1e0b6d4595848be3faaf4
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-09-18 23:58:11 +0200

    Improve the public method of Display

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -104,13 +104,16 @@ class Rectangle(Base):
 
     def display(self):
         """
-        Print the Rectangle instance with the character # to stdout.
+        Print the Rectangle instance with the character # to stdout,
+        accounting for x and y coordinates.
 
         Returns:
             None
         """
+        for _ in range(self.__y):
+            print()
         for _ in range(self.__height):
-            print("#" * self.__width)
+            print(" " * self.__x + "#" * self.__width)
 
     def __str__(self):
         """
