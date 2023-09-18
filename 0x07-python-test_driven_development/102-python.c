2c9023431fb88e8acd0a9c3c3519b891fba908f6
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-09-18 16:52:59 +0200

    Print Python Strings

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -11,23 +11,26 @@
 
 void print_python_string(PyObject *p)
 {
-	long int length;
+	Py_ssize_t length;
+	const wchar_t *wide_str = PyUnicode_AsWideCharString(p, &length);
 
-	fflush(stdout);
-
-	printf("[.] string object info\n");
-	if (strcmp(p->ob_type->tp_name, "str") != 0)
+	if (!p || !PyUnicode_Check(p))
 	{
-		printf("  [ERROR] Invalid String Object\n");
+		fprintf(stderr, "[ERROR] Invalid String Object\n");
 		return;
 	}
 
-	length = ((PyASCIIObject *)(p))->length;
+	printf("[.] string object info\n");
 
-	if (PyUnicode_IS_COMPACT_ASCII(p))
-		printf("  type: compact ascii\n");
-	else
-		printf("  type: compact unicode object\n");
-	printf("  length: %ld\n", length);
-	printf("  value: %ls\n", PyUnicode_AsWideCharString(p, &length));
+	if (!wide_str)
+	{
+		fprintf(stderr, "[ERROR] Failed to retrieve wide char string\n");
+		return;
+	}
+
+	printf("  type: %s\n", PyUnicode_IS_COMPACT_ASCII(p) ? "compact ascii" : "compact unicode object");
+	printf("  length: %zd\n", length);
+	printf("  value: %ls\n", wide_str);
+
+	PyMem_Free((void *)wide_str);
 }
