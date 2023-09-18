6196ced1c78e9b36b13737549e59c9f9937a776a
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-09-18 16:54:07 +0200

    Print Python Strings

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -13,6 +13,7 @@ void print_python_string(PyObject *p)
 {
 	Py_ssize_t length;
 	const wchar_t *wide_str = PyUnicode_AsWideCharString(p, &length);
+	fflush(stdout);
 
 	if (!p || !PyUnicode_Check(p))
 	{
