926296bca5497a9d576da06cd5fafe05d5ce2a3d
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-09-18 16:29:15 +0200

    Print Python Strings

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -9,16 +9,35 @@
  * If @p is not a valid string, it sets a Python exception.
 */
 
-void print_python_string(PyObject *p) {
-    if (!PyUnicode_Check(p)) {
-        PyErr_SetString(PyExc_TypeError, "p must be a valid string");
-        return;
-    }
+void print_python_string(PyObject *p)
+{
+    Py_ssize_t length;
+    const wchar_t *wide_str;
 
-    const char *str = PyUnicode_AsUTF8(p);
-    if (str) {
-        printf("String: %s\n", str);
-    } else {
-        PyErr_Print();
+    if (PyUnicode_Check(p))
+    {
+        printf("[.] string object info\n");
+
+        if (PyUnicode_IS_COMPACT_ASCII(p))
+            printf("  type: compact ascii\n");
+        else
+            printf("  type: compact unicode object\n");
+
+        wide_str = PyUnicode_AsWideCharString(p, &length);
+
+        if (wide_str != NULL)
+        {
+            printf("  length: %zd\n", length);
+            printf("  value: %ls\n", wide_str);
+            PyMem_Free((void *)wide_str);
+        }
+        else
+        {
+            printf("  [ERROR] Failed to retrieve wide char string\n");
+        }
+    }
+    else
+    {
+        printf("  [ERROR] Invalid String Object\n");
     }
 }
