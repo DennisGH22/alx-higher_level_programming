abe29eedf424820b45fe6a72eddfbb64a5cc8ca0
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-09-19 02:01:22 +0200

    Base tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -10,6 +10,49 @@ class TestBase(unittest.TestCase):
     """
     Unittest for Base
     """
+    def test_id_int(self):
+        """ Test integer id """
+        b = Base(5)
+        self.assertEqual(b.id, 5)
+        b = Base(0)
+        self.assertEqual(b.id, 0)
+        b = Base(-3)
+        self.assertEqual(b.id, -3)
+
+    def test_id_incrementation(self):
+        """ Test id incrementation """
+        Base._Base__nb_objects = 0
+        b = Base()
+        self.assertEqual(b.id, 1)
+        b = Base(7)
+        self.assertEqual(b.id, 7)
+        b = Base(None)
+        self.assertEqual(b.id, 2)
+        b = Base()
+        self.assertEqual(b.id, 3)
+
+    def test_id_non_int(self):
+        """ Test non integer id """
+        b = Base("Hello")
+        self.assertEqual(b.id, "Hello")
+        b = Base('A')
+        self.assertEqual(b.id, 'A')
+        b = Base([1, 2, 3])
+        self.assertEqual(b.id, [1, 2, 3])
+        b = Base((1, 2))
+        self.assertEqual(b.id, (1, 2))
+        b = Base({"id": 7, "name": "Betty"})
+        self.assertEqual(b.id, {"id": 7, "name": "Betty"})
+        b = Base(False)
+        self.assertEqual(b.id, False)
+
+    def test_id_error(self):
+        """ Test error """
+        with self.assertRaises(TypeError):
+            b = Base(1, 2)
+        with self.assertRaises(TypeError):
+            b = Base(1, None)
+
     def test_creation_id(self):
         """
         Test value assignment
