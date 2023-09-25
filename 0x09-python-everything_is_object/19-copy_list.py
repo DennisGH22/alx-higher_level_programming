5e65a805ec78bf20e39fe357d2490b0b8ddbf5dc
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-09-25 23:07:33 +0200

    Copy a list object

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,15 +1,3 @@
 #!/usr/bin/python3
-"""Returns a copy of the input list."""
-
-
 def copy_list(input_list):
-    """
-    Returns a copy of the input list.
-
-    Args:
-        l (list): The input list to be copied.
-
-    Returns:
-        list: A copy of the input list.
-    """
     return input_list[:]
