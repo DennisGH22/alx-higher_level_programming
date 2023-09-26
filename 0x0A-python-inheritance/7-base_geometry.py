d8c344907761a4ef8e616733bf7401803278d425
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-09-26 22:19:16 +0200

    Fix PEP8 guidelines errors

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -4,7 +4,7 @@
 
 class BaseGeometry:
     """BaseGeometry class."""
-    
+
     def area(self):
         """Raise an Exception with the message 'area() is not implemented'."""
         raise Exception("area() is not implemented")
