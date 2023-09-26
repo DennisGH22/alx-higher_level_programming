14eb738ec98a7b46590ebada9c972518f2d85c99
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-09-26 09:54:06 +0200

    Integer validator

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" The class represents a Base Geometry. """
+
+
+class BaseGeometry:
+    """BaseGeometry class."""
+    
+    def area(self):
+        """Raise an Exception with the message 'area() is not implemented'."""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validate an integer value."""
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
