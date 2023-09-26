ef97b6dacc4ce793b0d711ca206e88b1fef913ed
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-09-26 09:28:48 +0200

    Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" The class represents a Base Geometry. """
+
+
+class BaseGeometry:
+    """BaseGeometry class."""
+
+    def area(self):
+        """Raise an Exception with the message 'area() is not implemented'."""
+        raise Exception("area() is not implemented")
