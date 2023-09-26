f0306fa824c81797c73eced89c0c2be7ed4bb27b
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-09-26 22:17:01 +0200

    Check type using type()

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,27 +1,22 @@
 #!/usr/bin/python3
-"""BaseGeometry class."""
-
-
-class BaseGeometry:
-    """BaseGeometry class."""
-
-    def area(self):
-        """Raise an Exception with the message 'area() is not implemented'."""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """Validate an integer value."""
-        if not isinstance(value, int):
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+"""
+This module defines a class Rectangle that inherits from BaseGeometry.
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """Rectangle class, inherits from BaseGeometry."""
+    """
+    Rectangle class, inherits from BaseGeometry.
+    """
 
     def __init__(self, width, height):
-        """Initialize a Rectangle instance with width and height."""
+        """
+        Initialize a Rectangle instance with width and height.
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+        """
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
