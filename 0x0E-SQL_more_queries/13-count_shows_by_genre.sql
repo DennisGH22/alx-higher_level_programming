28db26aae4d80c72ccd560fbb2b3c4764a4a0264
Tebogo Ndaba <dennistebogo@gmail.com>
2023-10-19 00:53:48 +0200

    List all genres from hbtn_0d_tvshows and displays the number of shows linked to each

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,5 @@
+-- List all genres from hbtn_0d_tvshows and displays the number of shows linked to each
+SELECT tv_show_genres.genre AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
+FROM tv_show_genres
+GROUP BY genre
+ORDER BY number_of_shows DESC;
