673c7a275fbfbc1af057e0205dde3a786d65be21
Tebogo Ndaba <dennistebogo@gmail.com>
2023-10-19 00:15:51 +0200

    Create the MySQL server user user_0d_1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,8 +1,8 @@
 -- Create the MySQL server user user_0d_1
-CREATE OR REPLACE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+CREATE OR REPLACE USER user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
 
 -- Grant all privileges to the user
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
 
 -- Flush privileges to apply the changes
 FLUSH PRIVILEGES;
