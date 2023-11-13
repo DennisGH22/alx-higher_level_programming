6db82f5e0d10f4087e50e62419c982a852963b29
Tebogo Ndaba <dennistebogo@gmail.com>
2023-11-13 20:16:23 +0200

    Semistandard

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -2,5 +2,5 @@
 const arrayStrings = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (let i = 0; i < arrayStrings.length; i++) {
-	console.log(arrayStrings[i]);
+  console.log(arrayStrings[i]);
 }
