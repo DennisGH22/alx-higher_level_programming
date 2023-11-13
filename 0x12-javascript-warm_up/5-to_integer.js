6c5dc4150a8d05df3769d4483e032c2f8628c3c1
Tebogo Ndaba <dennistebogo@gmail.com>
2023-11-13 20:02:56 +0200

    Convert the first argument to int

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const args = process.argv.slice(2);
+
+if (!isNaN(parseInt(args[0], 10))) {
+  console.log(`My number: ${parseInt(args[0], 10)}`);
+} else {
+  console.log('Not a number');
+}
