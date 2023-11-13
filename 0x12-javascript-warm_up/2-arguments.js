7d608e6bfee07f64e9680bb8750f63294231d47a
Tebogo Ndaba <dennistebogo@gmail.com>
2023-11-13 19:31:11 +0200

    Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,14 +1,13 @@
 #!/usr/bin/node
+const args = process.argv.slice(2);
 let message;
 
-argv.forEach(() => {
-  if (process.argv.slice(2).length === 0) {
-    message = 'No argument';
-  } else if (process.argv.slice(2).length === 1) {
-    message = 'Argument found';
-  } else {
-    message = 'Arguments found';
-  }
-});
+if (args.length === 0) {
+  message = 'No argument';
+} else if (args.length === 1) {
+  message = 'Argument found';
+} else {
+  message = 'Arguments found';
+}
 
 console.log(message);
