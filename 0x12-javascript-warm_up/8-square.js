5ddeab28c806e9a385b899d913b021f9de66605e
Tebogo Ndaba <dennistebogo@gmail.com>
2023-11-13 21:05:22 +0200

    Print a square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const args = process.argv.slice(2);
+const firstArgument = parseInt(args[0], 10);
+
+if (!isNaN(firstArgument)) {
+  for (let i = 0; i < firstArgument; i++) {
+	let square = '';
+	for (let j = 0; j < firstArgument; j++) {
+    	square += 'X';
+	}
+	console.log(square);
+  }
+} else {
+  console.log('Missing size');
+}
