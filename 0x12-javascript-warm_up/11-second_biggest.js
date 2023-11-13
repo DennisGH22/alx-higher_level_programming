de2b8f00fd4ccdf794fa94ec6af72b1b8e178e53
Tebogo Ndaba <dennistebogo@gmail.com>
2023-11-13 22:50:59 +0200

    Search the second biggest integer

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const args = process.argv.slice(2);
+
+if (args.length === 0 || args.length === 1) {
+	console.log('0');
+} else {
+	let sorted = args.sort();
+	console.log(sorted[sorted.length - 2]);
+}
