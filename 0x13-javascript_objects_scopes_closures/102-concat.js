70b66fea51b27143df736f9ef4a52690110c9d9a
Tebogo Ndaba <dennistebogo@gmail.com>
2023-11-15 01:03:37 +0200

    Concat files

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+const fs = require('fs');
+
+function concatFiles (file1, file12, destination) {
+  const file1Content = fs.readFileSync(file1, 'utf-8');
+
+  const file2Content = fs.readFileSync(file12, 'utf-8');
+
+  const concatenatedContent = `${file1Content}\n${file2Content}`;
+
+  fs.writeFileSync(destination, concatenatedContent);
+
+  console.log(concatenatedContent);
+}
+
+const file1 = process.argv[2];
+const file12 = process.argv[3];
+const destination = process.argv[4];
+
+concatFiles(file1, file12, destination);
