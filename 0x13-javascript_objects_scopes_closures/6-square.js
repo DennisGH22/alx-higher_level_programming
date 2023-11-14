77aed2613e5e7b538b3c716d8257ed29141e71c9
Tebogo Ndaba <dennistebogo@gmail.com>
2023-11-14 23:12:53 +0200

    Square #1

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 module.exports = class Square extends require('./5-square') {
-  charPrint(c) {
+  charPrint (c) {
     if (c === undefined) {
       c = 'X';
     }
 
     for (let i = 0; i < this.height; i++) {
       console.log(c.repeat(this.width));
-	}
+    }
   }
-}
+};
