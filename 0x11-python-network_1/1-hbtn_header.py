7b6c56cc6c03b7489714216457049ff67fffe43f
Tebogo Ndaba <dennistebogo@gmail.com>
2023-12-01 16:47:02 +0200

    Response header value

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-import urllib.request
-import sys
-
 """
 Sends a request to the URL and displays the value of the X-Request-Id
 """
+import urllib.request
+import sys
 
 url = sys.argv[1]
 
