7200c42ded27160c1d719c30c35e6209a5542dbb
Tebogo Ndaba <dennistebogo@gmail.com>
2024-01-15 14:41:45 +0200

    Fetch and print how to say “Hello” depending on the language

--- 0x15-javascript-web_jquery/102-script.js
+++ 0x15-javascript-web_jquery/102-script.js
@@ -1,14 +1,14 @@
 $(document).ready(function () {
-	$('INPUT#btn_translate').click(function () {
-		let languageCode = $('INPUT#language_code').val();
-		
-		function sayHello (translate) {
-			$('DIV#hello').text(translate);
-		}
+  $('INPUT#btn_translate').click(function () {
+    const languageCode = $('INPUT#language_code').val();
 
-		$.get(`https://hellosalut.stefanbohacek.dev/?lang=${languageCode}`, function (data, displayTranslation) {
-			let translate = data.hello;
-			displayTranslation = sayHello(translate);
-		});
-	});
+    function sayHello (translate) {
+      $('DIV#hello').text(translate);
+    }
+
+    $.get(`https://hellosalut.stefanbohacek.dev/?lang=${languageCode}`, function (data, displayTranslation) {
+      const translate = data.hello;
+      displayTranslation = sayHello(translate);
+    });
+  });
 });
