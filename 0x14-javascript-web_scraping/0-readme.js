2bf0a18ac73b960259c0674de67f907dc7fc7808
Tebogo Ndaba <dennistebogo@gmail.com>
2024-01-25 16:00:13 +0200

    Initial commit

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-function readAndPrintFile(filePath) {
-    fs.promises.readFile(filePath, 'utf-8')
-        .then((data) => {
-            console.log(data);
-        })
-        .catch((err) => {
-            console.error(`An error occurred: ${err}`);
-        });
+function readAndPrintFile (filePath) {
+  fs.promises.readFile(filePath, 'utf-8')
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((err) => {
+      console.error(`An error occurred: ${err}`);
+    });
 }
 
 const filePath = process.argv[2];
