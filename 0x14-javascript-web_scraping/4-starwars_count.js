1ffffdd03a203bcbc0916ebf4bf909b03772034b
Tebogo Ndaba <dennistebogo@gmail.com>
2024-01-25 16:39:18 +0200

    Star wars Wedge Antilles

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+const request = require('request');
+
+function countMoviesWithCharacter (apiUrl, characterId) {
+  request(apiUrl, (error, response, body) => {
+    if (error) {
+      console.error(`Error: ${error.message}`);
+    } else if (response.statusCode !== 200) {
+      console.error(`Request failed with status code: ${response.statusCode}`);
+    } else {
+      const films = JSON.parse(body).results;
+      const moviesWithWedge = films.filter((film) => {
+        return film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`);
+      });
+      console.log(moviesWithWedge.length);
+    }
+  });
+}
+
+const [,, apiUrl] = process.argv;
+countMoviesWithCharacter(apiUrl, 18);
