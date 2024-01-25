f03f9e02e2fdaa0efa470f8502559519f94b717f
Tebogo Ndaba <dennistebogo@gmail.com>
2024-01-25 16:14:44 +0200

    Write a string to a file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 function writeToFile (filePath, content) {
   fs.promises.writeFile(filePath, content, 'utf-8')
     .then(() => {
-      return;
+
     })
     .catch(error => {
       console.error(`An error occurred while writing to ${filePath}: ${error.message}`);
